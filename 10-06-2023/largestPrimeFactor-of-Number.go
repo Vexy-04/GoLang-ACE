5073c0765436a4ad898a3ad73297c01a91b88318
Varun A L <79893116+Vexy-04@users.noreply.github.com>
2023-06-10 09:40:46 +0000

    update

---
+++ 10-06-2023/largestPrimeFactor-of-Number.go
@@ -0,0 +1,21 @@
+//largest prime factor of a number program in go
+package main
+import {
+	"fmt"
+	"math"
+}
+func largestPrimFactor(n int) int {
+	largest := 1
+
+	for n % 2 == 0 {
+		largest = 2
+		n = n/2
+	}
+
+	for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
+		for n % i == 0 {
+			largest = i
+			n = n/i
+		}
+	}
+}
