fdc556086e4b258551a80e281f6b757a0e27ae2b
Vexy-04 <varunal2003@gmail.com>
2023-06-28 16:13:25 +0530

    typo

--- 28-06-2023/Interface.go
+++ 28-06-2023/Interface.go
@@ -1,35 +1,39 @@
 package main
 
-import(
+import (
 	"fmt"
 	"math"
 )
 
-type Shape Interface{
+type Shape interface {
 	Area() float64
 }
 
-type Circle struct{
+type Circle struct {
 	radius float64
 }
 
-func (c Circle)Area()float64{
-	return  math.Pi*c.radius*c.radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
-type Rectangle struct{
-	width float64
+type Rectangle struct {
+	width  float64
 	height float64
 }
 
-func (r Rectangle)Area()float64{
-	return r.width*r.height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
 }
 
-func main(){
-	Circle := Circle{radius: 5}
+func PrintArea(s Shape) {
+	fmt.Printf("Area : %0.2f\n", s.Area())
+}
+
+func main() {
+	circle := Circle{radius: 5}
 	Rectangle := Rectangle{width: 4, height: 6}
 
-	PrintArea(Circle)
+	PrintArea(circle)
 	PrintArea(Rectangle)
 }
